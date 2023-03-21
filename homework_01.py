63d1ab0923b055abe01a94c25677d78dd4926acd
Viktorenkov <128530621+Viktorenkov@users.noreply.github.com>
2023-03-21 22:14:12 +0200

    Add files via upload

---
+++ homework_01.py
@@ -0,0 +1,32 @@
+# #Task_1 Створіть дві змінні first=10, second=30. Виведіть на екран результат математичної взаємодії (+, -, *, / и тд.) для цих чисел.
+#
+# first = 10
+# second = 30
+#
+# print(first+second)
+# print(first-second)
+# print(first*second)
+# print(first/second)
+#
+# #Task_2 Створіть змінну и по черзі запишіть в неї результат порівняння (<, > , ==, !=) чисел з завдання 1. Виведіть на екран результат кожного порівняння.
+#
+# compare_1 = first < second
+# compare_2 = first > second
+# compare_3 = first == second
+# compare_4 = first != second
+#
+# print(compare_1, compare_2, compare_3, compare_4)
+#
+# #Task_3 Створіть змінну - резкльтат конкатенації строк "Hello " та "world!".
+#
+# str_1 = 'Hello '
+# str_2 = 'world!'
+#
+# res = str_1 + str_2
+#
+# print(res)
+
+
+age = str(int(input('Введіть ваш вік:')))
+if '7' in age:
+    print('Вам сьогодні пощастить!')
